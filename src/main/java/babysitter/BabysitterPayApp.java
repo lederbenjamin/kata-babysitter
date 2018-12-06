30d256bce7189b244d7c00ed27da018c79268431
Ben Leder <lederbenjamin@gmail.com>
2018-12-06 17:40:21 -0500

    first test green

---
+++ src/main/java/babysitter/BabysitterPayApp.java
@@ -0,0 +1,10 @@
+package babysitter;
+
+public class BabysitterPayApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/babysitter/CalculatorTest.java
@@ -0,0 +1,16 @@
+package babysitter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		Calculator calculator = new Calculator();
+		assertNotNull(calculator);
+	}
+
+}

e970c9ac0eed9560fd38786cf6244373ade88174
Ben Leder <lederbenjamin@gmail.com>
2018-12-06 18:00:12 -0500

    second green

--- src/main/java/babysitter/Calculator.java
+++ src/main/java/babysitter/Calculator.java
@@ -1,5 +1,20 @@
 package babysitter;
 
+import java.math.BigDecimal;
+
 public class Calculator {
+
+	BigDecimal totalPay;
+	
+	public Calculator() {
+	}
+	
+	public BigDecimal getTotalPay() {
+		return totalPay;
+	}
+
+	public void setTotalPay(BigDecimal totalPay) {
+		this.totalPay = totalPay;
+	}
 
 }
--- src/test/java/babysitter/CalculatorTest.java
+++ src/test/java/babysitter/CalculatorTest.java
@@ -2,15 +2,34 @@ package babysitter;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class CalculatorTest {
+	
+	private Calculator calculator;
 
+	@Before
+	public void setup() {
+		calculator = new Calculator();
+	}
+
+	
 	@Test
-	public void test() {
-		Calculator calculator = new Calculator();
+	public void shouldInstansiateCalculatorObject() {
 		assertNotNull(calculator);
+	}
+	
+	@Test
+	public void shouldReturnTotalPay() {
+		BigDecimal fakePay = new BigDecimal(200);
+		
+		calculator.setTotalPay(fakePay);
+		
+		assertEquals(fakePay, calculator.getTotalPay());
 	}
 
 }
